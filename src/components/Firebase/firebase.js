091cc794c0ac5465c3dec7aa2448ec7adf10d806
muscaiu <muscaiu@yahoo.com>
2019-03-07 09:48:43 +0200

    add withFirebase and signup form

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -13,8 +14,25 @@ console.log('ENV = ', process.env.NODE_ENV)
 
 class Firebase {
   constructor() {
-    app.initializeApp(config)
+    app.initializeApp(config);
+
+    this.auth = app.auth();
   }
+
+  // *** Auth API ***
+
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = password =>
+    this.auth.currentUser.updatePassword(password);
 }
 
 export default Firebase;
