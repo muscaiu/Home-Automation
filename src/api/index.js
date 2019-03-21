ee6c864cdd28bc01eaf44ff7fb02b938c658cf40
Cristian Muscalu <muscaiu@yahoo.com>
2019-03-21 09:16:13 +0200

    reading sensor file

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,10 @@
 const logger = require('./logger');
+var fs = require('fs');
 
 const cronLiving = require('./cron/cronLiving');
 cronLiving.start()
 logger.info('cronLiving cron started');
+ 
+fs.readFile('sensor.log', 'utf8', function(err, sensor) {
+    console.log(sensor);
+});
